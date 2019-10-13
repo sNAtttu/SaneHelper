188a5fcd6dd609183f045b747c6667478e3cf87a
sNAttu <snattu91@gmail.com>
2019-10-13 23:04:44 +0300

    Location of assets can now be opened using cmd line app

--- SaneHelper/Services/FolderService.cs
+++ SaneHelper/Services/FolderService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using SaneHelper.Interfaces;
@@ -10,6 +11,9 @@ namespace SaneHelper.Services
     public class FolderService: IExecutable
     {
         private List<string> passedArguments;
+
+        private readonly string explorer = "explorer.exe";
+
         public FolderService(string[] args)
         {
             passedArguments = args.ToList();
@@ -17,8 +21,20 @@ namespace SaneHelper.Services
 
         public void ExecuteAction()
         {
-            Console.WriteLine("Opening folder: ");
-            Console.WriteLine(passedArguments[1]);
+            OpenFolder(passedArguments[1]);
         }
+
+        private void OpenFolder(string folder)
+        {
+            switch (folder)
+            {
+                case "humble":
+                    Process.Start(explorer, @"E:\HumbleBundle_GraphicAssets");
+                    break;
+                default:
+                    throw new ArgumentException("Invalid folder given in arguments");
+            }
+        }
+
     }
 }
